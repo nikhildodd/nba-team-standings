3d269e93397edda14de857eddcbed82262838813
Nikhil <ndstik@gmail.com>
2021-06-09 23:58:43 -0700

    had to change to free nba api

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ season = ""
 RAPIDAPI_KEY = MY_API_KEY
 RAPIDAPI_HOST = MY_API_HOST
 
-url = "https://api-nba-v1.p.rapidapi.com"
+url = "https://free-nba.p.rapidapi.com/teams"
 headers = {
     'x-rapidapi-key': RAPIDAPI_KEY,
     'x-rapidapi-host': RAPIDAPI_HOST
 }
+querystring = {"page":"0"}
 
 inputdata = {}
 inputdata["teams"] = []
@@ -31,19 +32,12 @@ inputdata["win-loss"] = []
 
 
 def fetchStandingsData(year):
-    response = requests.request("GET", url + "/standings/standard" + year, headers=headers)
-    
-    if response.code == 200:
-        return response.body
-    else:
-        return None
+    response = requests.request("GET", url + "/standings/standard" + year, headers=headers, params=querystring)
+    print('response text:', response.text)
 
 def fetchTeamNames(teamId):
-    response = requests.request("GET", url+"/teams/teamId"+teamId, headers)
-    if response.code == 200:
-        return response.body["api"]["teams"][0]["fullName"]
-    else:
-        return None
+    response = requests.request("GET", url+"/teams/teamId"+teamId, headers=headers, params=querystring)
+    print(response.text)
 
 if __name__ == "__main__":
     try:
@@ -58,8 +52,23 @@ if __name__ == "__main__":
             for team_data in standings_data:
                 print('team:', team_data)
                 team_name = fetchTeamNames(team_data["teamId"])
+
+                inputdata["teams"].append(team_name)
+                inputdata["result"].append("win")
+                inputdata["win-loss"].append(int(team_data["win"]))
+
+                inputdata['teams'].append(team_name)
+                inputdata['result'].append('loss')
+                inputdata['win-loss'].append(int(team_data['loss']))
+
+                print("Team:", team_name)
+                print("W-L: " + team_data['win'] + " - " + team_data['loss'])
+                print("\n")
+            
+            df = pd.DataFrame(inputdata)
+            print(df)
     
     except Exception as e:
-        print("Error")  
+        print("Errorhere")  
         print(e)
 
